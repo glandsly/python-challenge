6e28366feb2eecd4893a56a272ab7b357d95657e
George Landsly <2307210+glandsly@users.noreply.github.com>
2020-02-24 16:55:46 -0600

    cleaned and refined code, added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,42 @@
+# import dependencies
 import os
 import csv
 
-def print_output(output):
+# define a function that allows the program to print to both stdout and a text file
+def print_output(output, outfile):
 
-    with open('output.txt', 'a') as outfile:
-        print(output)
-        print(output, file=outfile)
+    print(output)
+    print(output, file=outfile)
 
+## main code block
+
+# set path for budget data
 budget_csv = os.path.join("..", "PyBank", "budget_data.csv")
 
+# initialize variables used in loops and conditionals
 total_months = 0
 net_revenue = 0
-last_month = 0
 monthly_change = []
-average_change = 0
-greatest_increase_date = "None"
 greatest_increase = 0
-greatest_decrease_date = "None"
 greatest_decrease = 0
 
+# open budget data csv file 
 with open(budget_csv) as budget_file:
 
     csvreader = csv.reader(budget_file, delimiter=',')
 
-    next(csvreader, None)
+    header = next(csvreader, None) # store header and skip it so it is not counted as data
 
+    # iterate through each row in the csv file, counting the row as a month and adding together the net revenue 
     for row in csvreader:
         total_months += 1
         net_revenue += int(row[1])
 
-        if total_months > 1:
+        if total_months > 1: # skip the first month since change needs at least two months of data to be calculated
             change = int(row[1]) - last_month
             monthly_change.append(change)
 
+            # as the csv is being iterated, find and store values of greatest change
             if change > greatest_increase:
                 greatest_increase = change
                 greatest_increase_date = row[0]
@@ -41,18 +45,19 @@ with open(budget_csv) as budget_file:
                 greatest_decrease = change
                 greatest_decrease_date = row[0]
 
-        last_month = int(row[1])
+        last_month = int(row[1]) # just before the loop restarts, temp store revenue data so it can be used to calculate change 
 
-    average_change = sum(monthly_change)/(total_months - 1)
+    average_change = sum(monthly_change)/len(monthly_change) # once the loop is complete, find the average change using the list of monthly change
 
-with open('output.txt', 'w') as output:
+# set output file and print output using custom print_output function
+with open('output.txt', 'w') as outfile:
 
-    print_output("Financial Analysis")
-    print_output("----------------------------")
-    print_output(f"Total Months: {total_months}")
-    print_output(f"Total: {net_revenue}")
-    print_output(f"Average  Change: {average_change}")
-    print_output(f"Greatest Increase in Profits: {greatest_increase_date} ({greatest_increase})")
-    print_output(f"Greatest Decrease in Profits: {greatest_decrease_date} ({greatest_decrease})")
+    print_output("Financial Analysis", outfile)
+    print_output("----------------------------", outfile)
+    print_output(f"Total Months: {total_months}", outfile)
+    print_output(f"Total: {net_revenue}", outfile)
+    print_output(f"Average  Change: {average_change:.2f}", outfile)
+    print_output(f"Greatest Increase in Profits: {greatest_increase_date} ({greatest_increase})", outfile)
+    print_output(f"Greatest Decrease in Profits: {greatest_decrease_date} ({greatest_decrease})", outfile)
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,50 @@
+# import dependencies
 import os
 import csv
 
-def print_output(output):
+# define a function that allows the program to print to both stdout and a text file
+def print_output(output, outfile):
 
-    with open('output.txt', 'a') as outfile:
-        print(output)
-        print(output, file=outfile)
+    print(output)
+    print(output, file=outfile)
 
+## main code block
+
+# set path for polling data
 poll_csv = os.path.join("..", "PyPoll", "election_data.csv")
 
+# initialize variables used in loops and conditionals
 total_votes = 0
 candidates = {}
 
+# open polling data csv file
 with open(poll_csv) as poll_file:
 
     csvreader = csv.reader(poll_file, delimiter=',')
 
-    next(csvreader, None)
+    header = next(csvreader, None) # store header and skip it so it is not counted as data
 
+    # iterate through each row in the csv file, incrementing the total number of votes by one
     for row in csvreader:
         total_votes += 1
 
-        if row[2] not in candidates:
+        if row[2] not in candidates: # adds a new candidate and sets their votes at 1 (important to count the initializing row as a vote too)
             candidates[row[2]] = 1
         else:
-            candidates[row[2]] += 1
+            candidates[row[2]] += 1 # if a candidate has already been added, just increments their vote count by 1
 
-print_output("Election Results")
-print_output("-------------------------")
-print_output(f"Total Votes: {total_votes}")
-print_output("-------------------------")
+# set output file and print output using custom print_output function
+with open('output.txt', 'w') as outfile:
 
-for candidate, votes in candidates.items():
-    print_output(f"{candidate}: {(votes/total_votes)*100}% ({votes})")
+    print_output("Election Results", outfile)
+    print_output("-------------------------", outfile)
+    print_output(f"Total Votes: {total_votes}", outfile)
+    print_output("-------------------------", outfile)
 
-print_output("-------------------------")
-print_output(f"Winner: {max(candidates, key=candidates.get)}")
-print_output("-------------------------")
+    # iterates through dictionary, using key (candidate), value (votes), and total votes to provide snapshot of each candidate's performance 
+    for candidate, votes in candidates.items():
+        print_output(f"{candidate}: {(votes/total_votes)*100:.3f}% ({votes})", outfile)
+
+    print_output("-------------------------", outfile)
+    print_output(f"Winner: {max(candidates, key=candidates.get)}", outfile) # finds the max value by comparing dictionary values and returning the key
+    print_output("-------------------------", outfile)
